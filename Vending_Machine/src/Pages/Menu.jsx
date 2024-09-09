adac2ccd65fb1c39fa2751ae1c68f8c7e40bba28
rupesh3201 <rupesh.mandge3201@gmail.com>
2024-09-10 00:01:19 +0530

    adding Items type

--- Vending_Machine/src/Pages/Menu.jsx
+++ Vending_Machine/src/Pages/Menu.jsx
@@ -1,13 +1,27 @@
 import "../CSS/Menu.css";
 
 function Menu() {
-  return (
-    <>
-      <div className="menu-heading-div">
-        <h3>....🙏...</h3>
-        <h1>Peturam</h1>
-      </div>
-    </>
-  );
+    return (
+        <>
+            <div className="menu-page-container">
+                <div className="menu-heading-div">
+                    <h3>....🙏...</h3>
+                    <h1>Peturam</h1>
+                </div>
+                <div className="menu-product_colletion-div">
+                    <div className="waffers-section">
+
+                    </div>
+
+                    <div className="desserts">
+
+                    </div>
+                    <div className="beverages-section">
+
+                    </div>
+                </div>
+            </div>
+        </>
+    );
 }
 export default Menu;
